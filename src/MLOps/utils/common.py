949d9f9e2bc5a71688c3373112fa1a773e458199
arjuuuuunnnnn <arjunbanur27@gmail.com>
2024-05-30 09:48:23 +0530

    utils v3

--- src/MLOps/utils/common.py
+++ src/MLOps/utils/common.py
@@ -1,7 +1,7 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-from MLOps import logger
+from src.MLOps import logger
 import json
 import joblib
 from ensure import ensure_annotations
