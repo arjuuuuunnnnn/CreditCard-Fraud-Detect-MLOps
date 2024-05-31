6d9dc09af69d2d2b590dcb0c7c6229cc5bc94fd0
arjuuuuunnnnn <arjunbanur27@gmail.com>
2024-05-31 22:31:08 +0530

    feat: data validation complete

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from src.MLOps import logger
 from src.MLOps.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.MLOps.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
-STAGE_NAME = "Data Ingestion stage"
+
+STAGE_NAME = "Data Ingestion Stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
@@ -10,5 +12,15 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+STAGE_NAME = "Data Validation Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<< \n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
 
 
