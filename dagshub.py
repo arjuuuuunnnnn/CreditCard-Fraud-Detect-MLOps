1fbb5a5c85be029850a4e12a4778760466847dca
arjuuuuunnnnn <arjunbanur27@gmail.com>
2024-06-01 19:37:42 +0530

    update: docs of mlflow changed

--- dagshub.py
+++
@@ -1,7 +0,0 @@
-import dagshub
-dagshub.init(repo_owner='arjuuuuunnnnn', repo_name='CreditCard-Fraud-Detect-MLOps', mlflow=True)
-
-import mlflow
-with mlflow.start_run():
-  mlflow.log_param('parameter name', 'value')
-  mlflow.log_metric('metric name', 1)
