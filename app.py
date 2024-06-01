6b04fa302f3d3293fc60e586f1c6e2993943eb0f
arjuuuuunnnnn <arjunbanur27@gmail.com>
2024-06-01 20:47:10 +0530

    app.py flask

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request
+import os
+import numpy as np
+import pandas as pd
+from src.MLOps.pipeline.prediction import PredictionPipeline
+
+app = Flask(__name__)
+
+@app.route('/', methods=["GET"])
+def home():
+    return render_template("index.html")
+
+
