42ddb5b3e11ceec02bf4627e394030758df3b071
arjuuuuunnnnn <arjunbanur27@gmail.com>
2024-06-04 10:25:19 +0530

    fix: predict html

--- app.py
+++ app.py
@@ -52,6 +52,8 @@ def index():
             Amount = float(request.form["Amount"])
             Class = int(request.form["Class"])
 
+            
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port = 8080, debug = True)
